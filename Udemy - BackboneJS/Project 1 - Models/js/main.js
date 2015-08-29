5f68ee4f50f40ea36cb22bb31792f6605c95e191
jasonou <jasonou95@gmail.com>
2015-08-29 00:07:11 -0700

    BackboneJS Project 1 complete, starting Project 2

--- Udemy - BackboneJS/Project 1 - Models/js/main.js
+++ Udemy - BackboneJS/Project 1 - Models/js/main.js
@@ -17,7 +17,7 @@ var Song = Backbone.Model.extend({
     // Validation, if invalid, must return a string
     validate: function(attrs) {
         if(!attrs.title)
-            return "Title is required!";
+            return "Title is required";
     }
 });
 
@@ -41,11 +41,68 @@ var Animal = Backbone.Model.extend({
     }
 });
 
+// Overriding
 var Dog = Animal.extend({
     walk: function() {
+        Animal.prototype.walk.apply(this);
         console.log("Dog walking...");
     }
 });
 
 var dog = new Dog();
 dog.walk();
+
+//===== Section 3 - Models Project =====
+var Vehicle = Backbone.Model.extend({
+    // Initialized when web page/model is called
+    initialize: function() {
+        console.log("A new vehicle has been created.");
+    },
+    
+    // Server and ID
+    idAttribute: "carId",
+    
+    urlRoot: "/api/songs",
+    
+    // Start Method
+    start: function() {
+        console.log("Vehicle has started");
+    },
+    
+    // Validation, if invalid, must return a string
+    validate: function(attrs) {
+        if(!attrs.registrationNumber)
+            return "Registration Number is required";
+    }
+});
+
+// Car model that extends Vehicle and overrides start
+var Car = Vehicle.extend({
+    start: function() {
+        console.log("Car with registration number " + this.get("registrationNumber") + " started.");
+    }
+});
+
+// Initialised instance of car with attributes registrationNumber and color
+var car = new Car({
+    registrationNumber: "XLI887", 
+    color: "Blue"
+});
+// Checks if car is valid then starts the car
+if(car.isValid()) {
+    car.start();
+} else {
+    console.log(car.validationError);
+}
+
+// deletes registrationNumber and prints out error
+car.unset("registrationNumber");
+if(!car.isValid()) 
+    console.log(car.validationError);
+// sets back registrationNumber and checks for error
+car.set("registrationNumber", "XLI887");
+if(car.isValid()) {
+    car.start();
+} else {
+    console.log(car.validationError);
+}
