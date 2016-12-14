c85092c907bed168461cb67c42fff08aa5afac07
jasonou <jasonou95@gmail.com>
2016-12-14 00:51:32 -0800

    Styled about page

--- Udemy - React Web Development/ReactWeather/app/components/About.jsx
+++ Udemy - React Web Development/ReactWeather/app/components/About.jsx
@@ -1,18 +1,25 @@
 var React = require('react');
 
-// var About = React.createClass({
-// 	render: function() {
-// 		return (
-// 			<h3>About Component</h3>
-// 		);
-// 	}
-// });
-
 var About = (props) => {
 	return (
 		<div>
-			<h3>About</h3>
-			<p>Welcome to the about page!</p>
+			<h1 className="text-center">About</h1>
+			<p>This is a weather application using React. I have built this for The Complete React Web App Developer Course.</p>
+			<p>Here are some of the tools that I used:</p>
+			<ul>
+				<li>
+					<a href="https://facebook.github.io/react/" target="_blank">React</a> - This was the JaveScript framework used.
+				</li>
+				<li>
+					<a href="http://openweathermap.org/" target="_blank">Open Weather Map</a> - I used Open Weather Map API to search for weather data by city name.
+				</li>
+				<li>
+					<a href="http://foundation.zurb.com/" target="_blank">Foundation</a> - I used Foundation for responsive design.
+				</li>
+				<li>
+					<a href="https://www.heroku.com/" target="_blank">Heroku</a> - I used Heroku for hosting.
+				</li>
+			</ul>
 		</div>
 	);
 }
