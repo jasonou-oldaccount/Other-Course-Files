1b57addd6e8435fb1ef92c077426845d2ddc4d1e
jasonou <jasonou95@gmail.com>
2016-12-13 23:46:51 -0800

    Added functionality for Heroku

--- Udemy - React Web Development/ReactWeather/server.js
+++ Udemy - React Web Development/ReactWeather/server.js
@@ -3,6 +3,15 @@ var express = require('express');
 
 // Create our app
 var app = express();
+const PORT = process.env.PORT || 3000;
+
+app.use(function (req, res, next) {
+	if(req.headers['x-forwarded-proto'] === 'https') {
+		res.redirect('http://' + req.hostname + req.url);
+	} else {
+		next();
+	}
+});
 
 // Tell server which folder to use
 // @param (folder_name)
@@ -10,6 +19,6 @@ app.use(express.static('public'));
 
 // Starts the server
 // @param (port, action)
-app.listen(3000, function() {
-	console.log('Express server is up on port 3000');
+app.listen(PORT, function() {
+	console.log('Express server is up on port ' + PORT);
 });
