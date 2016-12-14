b4a4c05f5975146f5cc61a5ff0b5dfcb0cc9d45d
jasonou <jasonou95@gmail.com>
2016-12-14 00:40:30 -0800

    Added grid systems and style examples

--- Udemy - React Web Development/ReactWeather/app/components/Examples.jsx
+++ Udemy - React Web Development/ReactWeather/app/components/Examples.jsx
@@ -1,18 +1,19 @@
 var React = require('react');
-
-// var Examples = React.createClass({
-// 	render: function() {
-// 		return (
-// 			<h3>Examples Component</h3>
-// 		);
-// 	}
-// });
+var {Link} = require('react-router');
 
 var Examples = (props) => {
 	return (
 		<div>
-			<h3>Examples</h3>
-			<p>Welcome to examples page!</p>
+			<h1 className="text-center">Examples</h1>
+			<p>Here are a few example locations to try out:</p>
+			<ol>
+				<li>
+					<Link to='/?location=Oakland'>Oakland, CA</Link>
+				</li>
+				<li>
+					<Link to='/?location=sanmateo'>San Mateo, CA</Link>
+				</li>
+			</ol>
 		</div>
 	);
 }
